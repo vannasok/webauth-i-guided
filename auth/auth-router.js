e45a667ff8c5a3f6fc19084e3f8915cc3d621ead
vanna sok <vannasok@vannas-MBP.fios-router.home>
2019-11-11 09:52:13 -0800

    user added

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,34 +1,37 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
 
 const Users = require('../users/users-model.js');
 
 router.post('/register', (req, res) => {
-  let user = req.body;
+   let userAcc = req.body;
+   const hash = bcrypt.hashSync(userAcc.password, 12);
+   userAcc.password = hash;
 
-  Users.add(user)
-    .then(saved => {
-      res.status(201).json(saved);
-    })
-    .catch(error => {
-      res.status(500).json(error);
-    });
+   Users.add(userAcc)
+      .then(saved => {
+         res.status(201).json(saved);
+      })
+      .catch(error => {
+         res.status(500).json(error);
+      });
 });
 
 router.post('/login', (req, res) => {
-  let { username, password } = req.body;
+   let { username, password } = req.body;
 
-  Users.findBy({ username })
-    .first()
-    .then(user => {
-      if (user) {
-        res.status(200).json({ message: `Welcome ${user.username}!` });
-      } else {
-        res.status(401).json({ message: 'Invalid Credentials' });
-      }
-    })
-    .catch(error => {
-      res.status(500).json(error);
-    });
+   Users.findBy({ username })
+      .first()
+      .then(user => {
+         if (user) {
+            res.status(200).json({ message: `Welcome ${user.username}!` });
+         } else {
+            res.status(401).json({ message: 'Invalid Credentials' });
+         }
+      })
+      .catch(error => {
+         res.status(500).json(error);
+      });
 });
 
 module.exports = router;
